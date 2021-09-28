7ed0e8cd42a687b6e475d6aac59e7eb3815fdac7
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-29 00:00:19 +0530

    [VaibhavChavan]Added:Creating a Simple LinkedList

---
+++ UC1_LinkedList/UC1_CreateSimpleLL.cs
@@ -0,0 +1,42 @@
+﻿using Day14LinkedList.UC1_LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedList
+{
+    class UC1_CreateSimpleLinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
