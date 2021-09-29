078dfc2f8decfaf44f4ecbff59fd5ceb42a3104b
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-29 22:12:24 +0530

    [VaibhavChavan]Added:Delete 1st element from Linkedlist

---
+++ UC5_LinkedList/UC5_Del1stEle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedList.UC5_LinkedList
+{
+    class UC5_Del1stEle
+    {
+        internal Node head; 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; 
+            }
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+    }
+}
