09cb9b4f92105bc0b3d2a1ec8ab18a0334182dd9
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-29 21:20:16 +0530

    [VaibhavChavan]Added:Create Linkedlist by adding 30 to 56 between 70

---
+++ UC4_LinkedList/UC4_Add30in56n70.cs
@@ -0,0 +1,71 @@
+﻿using Day14LinkedList.UC4_LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedList.UC4_LinkedList
+{
+    class UC4_Add30in56n70
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            { 
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+    }
+}
